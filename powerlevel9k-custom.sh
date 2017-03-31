a18b553376df9c54f882239c3ab8de7071a02c13
andre.formento <andre.formento@concrete.com.br>
2017-03-31 14:46:54 -0300

    Update :)

--- powerlevel9k-custom.sh
+++ powerlevel9k-custom.sh
@@ -1,8 +1,11 @@
 # use -> add on .zshrc
 # source ~/ubuntu-local-configs/powerlevel9k-custom.sh
 
+# cp agnoster.zsh-theme ~/.oh-my-zsh/themes
+
 #ZSH_THEME="robbyrussell"
-ZSH_THEME="powerlevel9k/powerlevel9k"
+# ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="agnoster"
 TERM="xterm-256color"
 POWERLEVEL9K_DISABLE_RPROMPT=true
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
