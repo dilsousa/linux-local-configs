4eeda2ed62c7ebf8d929af62974d43e8c2be8474
andre.formento <andre.formento@concrete.com.br>
2017-03-31 14:13:30 -0300

    Update custom config

--- powerlevel9k-custom.sh
+++ powerlevel9k-custom.sh
@@ -1,8 +1,12 @@
 # use -> add on .zshrc
-# source /home/andre/ubuntu-local-configs/powerlevel9k-custom
+# source ~/ubuntu-local-configs/powerlevel9k-custom.sh
 
 #ZSH_THEME="robbyrussell"
 ZSH_THEME="powerlevel9k/powerlevel9k"
 TERM="xterm-256color"
 POWERLEVEL9K_DISABLE_RPROMPT=true
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+
+# POWERLINE_HIDE_USER_NAME="true"
+POWERLINE_HIDE_HOST_NAME="true"
+export DEFAULT_USER="$(whoami)"
