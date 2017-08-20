7c3b63e02aa5d23b554f3677a05ea7320115500b
André Formento <andreformento.sc@gmail.com>
2017-08-19 21:14:14 -0300

    Add docker kill

--- custom.sh
+++ custom.sh
@@ -5,6 +5,7 @@ alias g='git '
 
 # maven
 alias m='mvn clean install -U '
+alias dod='docker kill $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 
 alias wifi=watch -n 1 "awk 'NR==3 {print \"WiFi Signal Strength = \" \$3 \"00 %\"}''' /proc/net/wireless"
 
