17bdcda97015032a6394fe9591f454b55a6f646a
André Formento <andreformento.sc@gmail.com>
2017-11-03 11:02:06 -0200

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -7,6 +7,6 @@ alias g='git '
 alias m='mvn clean install -U '
 alias dod='~/linux-local-configs/docker-commands.sh'
 
-alias wifi=watch -n 1 "awk 'NR==3 {print \"WiFi Signal Strength = \" \$3 \"00 %\"}''' /proc/net/wireless"
+# alias wifi=watch -n 1 "awk 'NR==3 {print \"WiFi Signal Strength = \" \$3 \"00 %\"}''' /proc/net/wireless"
 
 # export MAVEN_OPTS="-Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000" mvn tomcat:run
