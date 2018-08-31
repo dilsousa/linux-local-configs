ce5c5d966ea7da0a63cb11200f1f05acd0ad72cc
André Formento <andreformento.sc@gmail.com>
2018-08-31 17:55:28 -0300

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -8,6 +8,8 @@ alias m='mvn clean install -U '
 alias dod='~/linux-local-configs/docker-commands.sh'
 alias update='sudo apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y'
 
+export PATH=$PATH:~/bin/lein
+
 # alias wifi=watch -n 1 "awk 'NR==3 {print \"WiFi Signal Strength = \" \$3 \"00 %\"}''' /proc/net/wireless"
 
 # export MAVEN_OPTS="-Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000" mvn tomcat:run
