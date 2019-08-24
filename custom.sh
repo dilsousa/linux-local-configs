d33ef6cd3b23b14e5eeaea8602c9de012581c8bc
André Formento <andreformento.sc@gmail.com>
2019-08-24 15:25:38 -0300

    Add functions

--- custom.sh
+++ custom.sh
@@ -10,6 +10,24 @@ alias update='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove
 alias wip="g add -A && g commit -m 'wip' && g ph"
 alias master="g checkout master && g pull"
 
+clone() {
+    GIT_PATH_NAME="$1"
+    if [ -z "${GIT_PATH_NAME}" ]; then
+        echo "Use 'clone path/repository'"
+    else
+        git clone "git@github.com:${GIT_PATH_NAME}" && cd "$(basename "${GIT_PATH_NAME}" .git)"
+    fi
+}
+
+ph() {
+    COMMENT="$1"
+    if [ -z "${COMMENT}" ]; then
+        echo "ph 'my comment about this commit'"
+    else
+        g add -A && g commit -m "${COMMENT}" && g ph
+    fi
+}
+
 export PATH=$PATH:~/bin/lein
 
 # alias wifi=watch -n 1 "awk 'NR==3 {print \"WiFi Signal Strength = \" \$3 \"00 %\"}''' /proc/net/wireless"
