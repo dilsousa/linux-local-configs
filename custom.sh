a8535d7d557a1e1032bcf210aa71a008e4e46e1b
André Formento <andreformento.sc@gmail.com>
2020-06-23 15:29:02 -0300

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -7,6 +7,8 @@ alias g='git '
 alias update='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y  && sudo apt autoclean -y'
 alias wip="g add -A && g commit -m 'wip' && g ph"
 alias master="g checkout master && g pull"
+alias docker-rm="docker rm -f $(docker ps -a -q)"
+alias docker-rmi="docker rmi -f $(docker images -q)"
 
 clone() {
     GIT_PATH_NAME="$1"
